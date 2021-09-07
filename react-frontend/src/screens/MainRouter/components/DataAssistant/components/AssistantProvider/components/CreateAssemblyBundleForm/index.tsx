207280f9b33e73604afca63a34d86949eea91f42
Archina <archina5thrider@gmail.com>
2021-09-07 14:13:18 +0200

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- react-frontend/src/screens/MainRouter/components/DataAssistant/components/AssistantProvider/components/CreateAssemblyBundleForm/index.tsx
+++ react-frontend/src/screens/MainRouter/components/DataAssistant/components/AssistantProvider/components/CreateAssemblyBundleForm/index.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import API from "../../../../../../../../api";
 import Button from "../../../../../../../../components/Button";
 import Input from "../../../../../../../../components/Input";
 
@@ -6,7 +7,15 @@ export function CreateAssemblyBundleForm(props: ICreateAssemblyProps): JSX.Eleme
     const [assemblies, setAssemblies] = useState<IAssembly[]>([]);
     const [draft, setDraft] = useState(defaultAssembly());
     const [selected, setSelected] = useState<number | null>(null);
+    const [possibleImports, setPossibleImports] = useState<{fasta: {[key: string]: string[][]}}>({fasta: {}});
+    const [fetching, setFetching] = useState(false);
+
+    useEffect(() => {
+        loadFiles(["image", "fasta", "gff", "bam", "analysis"]);
+    }, []);
     
+    const api = new API();
+
     function addDraftAssembly(){
         if(draft.name || draft.taxonId) {
             setAssemblies([...assemblies, draft]);
@@ -17,7 +26,48 @@ export function CreateAssemblyBundleForm(props: ICreateAssemblyProps): JSX.Eleme
     function defaultAssembly(): IAssembly {
         return {
             taxonId: 0,
-            name: ""
+            name: "",
+            files: []
+        }
+    }
+
+    async function loadFiles(types: ("image"|"fasta"|"gff"|"bam"|"analysis")[] | undefined = undefined) {
+        setFetching(true);
+        const response = await api.fetchPossibleImports(types);
+        if (response && response.payload) {
+            setPossibleImports(response.payload);
+        }
+
+        if (response && response.notification) {
+        //   handleNewNotification(response.notification);
+        }
+        setFetching(false);
+    };
+
+    function assemblyClassName(assemblyIdx: number): string {
+        return selected === assemblyIdx ?
+            "mt-4 animate-grow-y shadow p-4 rounded-lg w-64 bg-green-200" :
+            "mt-4 animate-grow-y shadow p-4 rounded-lg w-64";
+    }
+
+    function filesClassName(filePath: string): string {
+        if(selected !== null && assemblies[selected].files.some(fp => fp === filePath)) {
+            return "text-green-600 font-semibold";
+        } else {
+            return "";
+        }
+    }
+
+    function togglePathInSelectedAssembly(filePath: string){
+        if(selected !== null) {
+            const assems = [...assemblies];
+            assems.splice(selected, 1, {
+                ...assemblies[selected],
+                files: assemblies[selected].files.some(path => filePath === path) ?
+                assemblies[selected].files.filter(path => filePath !== path) :
+                [...assemblies[selected].files, filePath]
+            });
+            setAssemblies(assems);
         }
     }
 
@@ -25,7 +75,7 @@ export function CreateAssemblyBundleForm(props: ICreateAssemblyProps): JSX.Eleme
         <ul className="flex-2">
             {
                 assemblies.map((assembly, idx) => {
-                    return (<li className="mt-4 animate-grow-y shadow p-4 rounded-lg w-64" onClick={() => setSelected(idx)}>
+                    return (<li className={assemblyClassName(idx)} onClick={() => setSelected(idx)}>
                         <div className="flex items-center">
                             <span>{assembly.name} ({assembly.taxonId})</span>
                             {/* files */}
@@ -46,7 +96,11 @@ export function CreateAssemblyBundleForm(props: ICreateAssemblyProps): JSX.Eleme
         </ul>
         <div className="flex-1">
             Files - tree with checkmarks:
-
+            {<ul>
+                {Object.entries(possibleImports.fasta).map(([k, v]) =>
+                    v.map(vs => <li onClick={() => togglePathInSelectedAssembly(vs.join('/'))} className={filesClassName(vs.join('/'))}>{vs.join('/')}</li>)
+                )}
+            </ul>}
         </div>
     </div>);
 }
@@ -60,5 +114,6 @@ export type ICreateAssemblyProps = {}
 interface IAssembly{
     taxonId: number;
     name: string;
+    files: string[];
 }
 
