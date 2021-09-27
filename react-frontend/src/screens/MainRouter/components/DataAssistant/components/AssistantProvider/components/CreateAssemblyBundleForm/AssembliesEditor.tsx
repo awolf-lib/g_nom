a988692d1e2068a7f4ed2669cb0e0cbe36acc5dc
Archina <archina5thrider@gmail.com>
2021-09-27 05:34:02 +0200

    fix selected, add rename, fix indentation

--- react-frontend/src/screens/MainRouter/components/DataAssistant/components/AssistantProvider/components/CreateAssemblyBundleForm/AssembliesEditor.tsx
+++ react-frontend/src/screens/MainRouter/components/DataAssistant/components/AssistantProvider/components/CreateAssemblyBundleForm/AssembliesEditor.tsx
@@ -8,34 +8,57 @@ import { IAssembly } from "./_interfaces";
 
 export function AssembliesEditor(props: AssembliesEditorProps){
     const [draftName, setDraftName] = useState("");
-    const [selected, setSelected] = useState<number | null>(0);
+    const [selected, setSelected] = useState<number | null>(null);
+
+    function setAssemblyName(idx: number, name: string) {
+        props.onChange([...props.assemblies.map((a, i) =>
+            i === idx ? {...a, name} : a
+        )]);
+    }
 
     return (<div className="flex flex-row space-x-4">
-        <div>
-            {props.assemblies.map((assembly, idx) => <div className="p-4 shadow">
+        <div className="space-y-4">
+            {props.assemblies.map((assembly, idx) => <div className="p-4 shadow hover:bg-gray-200 rounded-lg" onClick={() => setSelected(idx)}>
                 {assembly.assemblyId !== null ?
-                    (<div className="flex items-center justify-between space-x-2" onClick={() => setSelected(idx)}><span>{assembly.name}</span></div>):
-                    (<div className="flex items-center justify-between space-x-2" onClick={() => setSelected(idx)}>
-                        <Input placeholder="AssemblyName" value={[assembly.name]} onChange={e => null} />
-                        <button onClick={() => props.onChange(props.assemblies.filter((a,i) => i !== idx))} title="remove taxon data bundle">
+                    (<div className="flex items-center justify-between space-x-2"><span>{assembly.name}</span></div>):
+                    (<div className="flex items-center justify-between space-x-2">
+                        <Input
+                            placeholder="AssemblyName"
+                            value={[assembly.name]}
+                            onChange={e => setAssemblyName(idx, e.target.value)}
+                        />
+                        <button
+                            onClick={() => {
+                                setSelected(null);
+                                props.onChange(props.assemblies.filter((_,i) => i !== idx))
+                            }}
+                            title="remove taxon data bundle"
+                        >
                             <Trash color="red" className="stroke-current" />
                         </button>
                     </div>)
                 }
             </div>)}
-            <div className="space-y-4 shadow p-4">
-                <Input placeholder="AssemblyName" value={[draftName]} onChange={(e: ChangeEvent<HTMLInputElement>) => setDraftName(e.target.value)} />
-                <Button onClick={() => {
-                    props.onChange([...props.assemblies, {
-                        assemblyId: null,
-                        name: draftName,
-                        path: null,
-                        analysis: null,
-                        annotation: null,
-                        mapping: null,
-                    }]);
-                    setDraftName("");
-                }} disabled={draftName.length === 0}>Add Assembly</Button>
+            <div className="space-y-4 shadow p-4 rounded-lg">
+                <Input
+                    placeholder="AssemblyName"
+                    value={[draftName]}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setDraftName(e.target.value)}
+                />
+                <Button
+                    onClick={() => {
+                        props.onChange([...props.assemblies, {
+                            assemblyId: null,
+                            name: draftName,
+                            path: null,
+                            analysis: null,
+                            annotation: null,
+                            mapping: null,
+                        }]);
+                        setDraftName("");
+                    }}
+                    disabled={draftName.length === 0}
+                >Add Assembly</Button>
             </div>
         </div>
         <div className="flex-grow">{ selected !== null ?
