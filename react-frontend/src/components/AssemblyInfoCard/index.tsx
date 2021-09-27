f5fb47797ad396ead9a447e8aef5ef26cf82e9e3
Archina <archina5thrider@gmail.com>
2021-09-27 04:52:28 +0200

    fix most typescript warnings

--- react-frontend/src/components/AssemblyInfoCard/index.tsx
+++ react-frontend/src/components/AssemblyInfoCard/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App.css";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 import SpeciesProfilePictureViewer from "../SpeciesProfilePictureViewer";
