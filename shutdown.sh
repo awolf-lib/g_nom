0e5975d3a1687325996cd1dac5d65b688e28560d
awolf-lib <andywol93@gmail.com>
2021-10-06 14:45:31 +0200

    shutdown.sh: add stop react container

--- shutdown.sh
+++ shutdown.sh
@@ -7,3 +7,7 @@ killall screen
 # stop docker container
 MYSQL_CONTAINER_NAME=$(grep "MYSQL_CONTAINER_NAME" config.txt | cut -f2 -d "=")
 docker stop $MYSQL_CONTAINER_NAME
+
+# stop React Docker container
+REACTAPP_CONTAINER_NAME=$(grep "REACTAPP_CONTAINER_NAME" config.txt | cut -f2 -d "=")
+docker stop $REACTAPP_CONTAINER_NAME
