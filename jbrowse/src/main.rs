b9ee026263a706450a89d1c5cb883412b051c245
Archina <archina5thrider@gmail.com>
2021-10-26 01:15:01 +0200

    model resource messages and handle assembly added, change jbrowse directory

--- jbrowse/src/main.rs
+++ jbrowse/src/main.rs
@@ -1,31 +1,20 @@
 use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions, Result};
-use serde::{Deserialize, Serialize};
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 
-#[derive(Deserialize, Serialize, Debug)]
-struct Assembly{
-    name: String,
-    id: String
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct NewAssemblyMessage{
-    assembly: Assembly,
-    storage_path: String
-}
+mod message;
 
 const STORAGE_ROOT: &str = "/";
 const JBROWSE_PATH: &str = "/usr/local/apache2/htdocs";
 
-fn handle_new_assembly(message: &NewAssemblyMessage){
+fn handle_new_assembly(message: &message::AssemblyMessage) -> Result<ExitStatus, std::io::Error>{
     let storage_fasta = format!("{}{}", STORAGE_ROOT, &message.storage_path);
 
-    Command::new("samtools")
+    let samtolls = Command::new("samtools")
         .arg("faidx")
         .arg(&storage_fasta)
-        .status().expect("Couldn't execute samtools on {}");
+        .status();
 
-    Command::new("jbrowse")
+    let jbrowse = Command::new("jbrowse")
         .arg("add-assembly")
         .arg(&storage_fasta)
         .arg("--load")
@@ -33,16 +22,17 @@ fn handle_new_assembly(message: &NewAssemblyMessage){
         .arg("--name")
         .arg(&message.assembly.name)
         .arg("--target")
-        .arg(&JBROWSE_PATH)
-        .status()
-        .expect("Couldn't add-assembly");
+        .arg(format!("{}/{}", &JBROWSE_PATH, &message.assembly.name))
+        .status();
+
+    samtolls.and(jbrowse)
 }
 
 fn main() -> Result<()> {
     let rabbit_host = "amqp://guest:guest@gnom_rabbit:5672";
     let mut connection = Connection::insecure_open(&rabbit_host).expect("RabbitMQ Connection not established");
     let channel = connection.open_channel(None).expect("RabbitMQ cannot access channel");
-    let queue = channel.queue_declare("assembly", QueueDeclareOptions{
+    let queue = channel.queue_declare("resource", QueueDeclareOptions{
         durable: true,
         ..Default::default()
     }).expect("Cannot declare \"assembly\" queue.");
@@ -51,12 +41,22 @@ fn main() -> Result<()> {
     for message in consumer.receiver().iter() {
         match message {
             ConsumerMessage::Delivery(delivery) => {
-                if let Ok(message) = serde_json::from_slice::<NewAssemblyMessage>(&delivery.body) {
+                if let Ok(message) = serde_json::from_slice::<message::ResourceMessage>(&delivery.body) {
                     println!("{:?}", message);
-
-                    handle_new_assembly(&message);
-
-                    consumer.ack(delivery).expect("Couldn't acknowledge.");
+                    let output = match &message {
+                        message::ResourceMessage::Assembly(a) => handle_new_assembly(a),
+                        message::ResourceMessage::Mapping(_m) => Err(std::io::Error::new(std::io::ErrorKind::Other, "Mapping not handled yet...")),
+                        message::ResourceMessage::Annotation(_a) => Err(std::io::Error::new(std::io::ErrorKind::Other, "Annotation not handled yet...")),
+                    };
+                    match output {
+                        Ok(_) => {
+                            consumer.ack(delivery).expect("Couldn't acknowledge.");
+                        },
+                        Err(error) => {
+                            println!("Rejected: {:?}", error);
+                            consumer.reject(delivery, false).expect("Couldn't reject.");    
+                        },
+                    }
                 } else {
                     println!("Received none-NewAssembly message");
                     consumer.reject(delivery, false).expect("Couldn't reject.");
