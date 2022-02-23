29a4f34c8d5344e59090fcefc84829539bd2656b
awolf-lib <andywol93@gmail.com>
2022-02-23 23:47:16 +0100

    replace login logo

--- react-frontend/src/screens/MainRouter/components/Login/index.js
+++ react-frontend/src/screens/MainRouter/components/Login/index.js
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import "../../../../App.css";
-
 import Button from "../../../../components/Button";
-
 import { login } from "../../../../api";
-
-// import DNA from "../../../../images/DNA.gif";
 import { useNotification } from "../../../../components/NotificationProvider";
 import Input from "../../../../components/Input";
 
@@ -48,36 +44,64 @@ const Login = ({ setToken, setUserID, setUserRole, setUserName }) => {
     });
   };
 
+  const DnaLength = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+  ];
+
   return (
-    <div className="flex justify-center w-full items-center bg-gradient-to-tr from-gray-900 via-gray-500 to-gray-700 h-screen">
-      {/* <div
-        style={{ maxWidth: "90vh" }}
-        className="bg-transparent rounded-full transform rotate-90 overflow-hidden object-contain border-4 border-double border-gray-300"
-      >
-        <img alt="Login DNA GIF" src={DNA} />
-      </div> */}
+    <div className="flex justify-around w-full h-full items-center bg-gradient-to-tr from-gray-900 via-gray-500 to-gray-700 h-screen">
+      <div className="hidden lg:block h-2/3 w-1/4 overflow-hidden bg-gradient-to-tr from-gray-300 via-gray-100 to-gray-200 rounded-lg shadow border-4 border-gray-300 border-double">
+        <div className="w-full h-full flex justify-center items-center relative bg-gray-300">
+          <div className="rotate-45 translate-x-6">
+            {DnaLength.map((element) => (
+              <div
+                className="line"
+                style={{
+                  marginTop: "15px",
+                  animationDelay: element * 0.2 + "s",
+                  animationDuration: "2s",
+                }}
+              ></div>
+            ))}
+          </div>
+          <div className="absolute right-0 bottom-0 mx-4 my-2 border-b border-gray-400 font-semibold">
+            from Sequence to Function
+          </div>
+        </div>
+      </div>
+
       <form
         onSubmit={(e) => handleSubmit(e)}
-        className="bg-gradient-to-tr z-20 w-1/2 from-gray-300 via-gray-100 to-gray-200 px-16 py-8 rounded-lg shadow border-4 border-gray-300 border-double"
+        className="bg-gradient-to-tr flex justify-center items-center from-gray-300 via-gray-100 to-gray-200 px-16 py-8 rounded-lg shadow border-4 border-gray-300 border-double"
       >
-        <h1 className="font-bold">Login to G-nom ...</h1>
-        <hr className="my-4 shadow" />
-        <div className="flex justify-between items-center">
-          <div className="mr-8 font-semiboldtext-left py-2 py-0 w-32">Username:</div>
-          <Input placeholder="Username..." onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div className="flex justify-between items-center pt-4">
-          <div className="mr-8 font-semiboldtext-left py-2 py-0 w-32">Password:</div>
-          <Input
-            placeholder="Password..."
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <hr className="my-4 shadow" />
         <div className="w-full">
-          <div>
-            <Button label="Login" size="sm" type="submit" />
+          <h1 className="font-bold">Login</h1>
+          <hr className="my-4 shadow" />
+          <div className="flex justify-between items-center">
+            <div className="hidden xl:block mr-8 font-semiboldtext-left py-2 py-0 w-32">
+              Username:
+            </div>
+            <div className="w-96">
+              <Input placeholder="Username..." onChange={(e) => setName(e.target.value)} />
+            </div>
+          </div>
+          <div className="flex justify-between items-center pt-4">
+            <div className="hidden xl:block mr-8 font-semiboldtext-left py-2 py-0 w-32">
+              Password:
+            </div>
+            <div className="w-96">
+              <Input
+                placeholder="Password..."
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <hr className="my-4 shadow" />
+          <div className="w-full flex justify-end">
+            <div className="w-32">
+              <Button label="Login" size="sm" type="submit" />
+            </div>
           </div>
         </div>
       </form>
